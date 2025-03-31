22c84a384822b0a3734d158d952b96b7d5bd7139
ananyag595 <ananyagupta595@gmail.com>
2025-03-31 17:54:42 +0530

    Made the clue interactable

--- BuildersCodex/BuildersCodex/Assets/BK_AlchemistHouse/Scripts/FreeCamera.cs
+++ BuildersCodex/BuildersCodex/Assets/BK_AlchemistHouse/Scripts/FreeCamera.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
 
-public class FreeCamera : MonoBehaviour{
+public class FreeCamera : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float lookSpeed = 2.0f; // Adjust mouse sensitivity
+    public float zoomSpeed = 5.0f;
+    public float minFOV = 20f;
+    public float maxFOV = 60f;
 
-	public float movementSpeed = 5.0f;
+    private Camera cam;
 
-	void FixedUpdate()
-	{
-		var horizontalAxis = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * movementSpeed;
-		var verticalAxis = Input.GetAxis("Vertical") * Time.fixedDeltaTime * movementSpeed;
-		var lookX = Input.GetAxis ("Mouse X");
-		var lookY = Input.GetAxis ("Mouse Y");
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        cam = GetComponent<Camera>();
+    }
 
-		transform.Translate(horizontalAxis, 0, 0);
-		transform.Translate(0, 0, verticalAxis);
-		transform.eulerAngles += new Vector3(-lookY, lookX, 0);
-	}
+    void FixedUpdate()
+    {
+        // Movement using WASD or arrow keys
+        var horizontalAxis = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * movementSpeed;
+        var verticalAxis = Input.GetAxis("Vertical") * Time.fixedDeltaTime * movementSpeed;
+        transform.Translate(horizontalAxis, 0, verticalAxis);
+
+        // Rotate only when right-clicking
+        if (Input.GetMouseButton(1))
+        {
+            var lookX = Input.GetAxis("Mouse X") * lookSpeed;
+            var lookY = Input.GetAxis("Mouse Y") * lookSpeed;
+            transform.eulerAngles += new Vector3(-lookY, lookX, 0);
+        }
+
+        // Zoom with scroll wheel
+        float scroll = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        if (cam != null)
+        {
+            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - scroll, minFOV, maxFOV);
+        }
+    }
 }
